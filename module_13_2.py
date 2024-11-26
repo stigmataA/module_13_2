61b55b94a9a9ed6bb9d208adbba96750ea38bc33
stigmataA <r.muhutdinov@gmail.com>
2024-11-26 16:35:40 +0300

    Add files via upload

--- module_13_2.py
+++ module_13_2.py
@@ -2,7 +2,7 @@ from aiogram import Bot, Dispatcher, executor, types
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
 import asyncio
 
-api = " "
+api = "7812244583:****"
 bot = Bot(token=api)
 dp = Dispatcher(bot, storage=MemoryStorage())
 
