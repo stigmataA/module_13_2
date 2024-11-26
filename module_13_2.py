fc5a1150f7add6874d3cd7b4b72011e4dd730a84
stigmataA <r.muhutdinov@gmail.com>
2024-11-26 16:26:27 +0300

    Add files via upload

---
+++ module_13_2.py
@@ -0,0 +1,20 @@
+from aiogram import Bot, Dispatcher, executor, types
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
+import asyncio
+
+api = "7812244583:AAHdfSD7zfxdC2lcIGdH6Q5FCoY_cop5Be0"
+bot = Bot(token=api)
+dp = Dispatcher(bot, storage=MemoryStorage())
+
+@dp.message_handler(commands=['start'])
+async def start_message(message):
+    print("Привет! Я бот помогающий твоему здоровью.")
+    #await message.answer("!")
+
+@dp.message_handler()
+async def all_message(message):
+    print("Введите команду /start, чтобы начать общение.")
+    #await message.answer(message.text.upper())
+
+if __name__ == '__main__':
+    executor.start_polling(dp, skip_updates=True)
